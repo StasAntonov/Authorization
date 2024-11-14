eae47c4705a60ffaba4a183803e2ea0fb253d174
StasAntonov <andropa07stas@gmail.com>
2024-11-14 21:11:10 +0200

    added network di

---
+++ app/src/main/java/com/example/authorization/di/NetworkModule.kt
@@ -0,0 +1,37 @@
+package com.example.authorization.di
+
+import com.example.authorization.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(okHttpClient)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideOkhttp(): OkHttpClient {
+        val builder = OkHttpClient.Builder()
+        if (BuildConfig.DEBUG) {
+            val logging =
+                HttpLoggingInterceptor().apply { setLevel(HttpLoggingInterceptor.Level.BODY) }
+            builder.addInterceptor(logging)
+        }
+        return builder.build()
+    }
+}
