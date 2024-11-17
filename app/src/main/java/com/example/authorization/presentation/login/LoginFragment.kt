b2859df517552a53fb06ea91ea7f091a4e75e5fe
StasAntonov <andropa07stas@gmail.com>
2024-11-17 17:49:08 +0200

    [AT-5] added validation

--- app/src/main/java/com/example/authorization/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/authorization/presentation/login/LoginFragment.kt
@@ -1,6 +1,57 @@
 package com.example.authorization.presentation.login
 
+import androidx.core.widget.doAfterTextChanged
+import androidx.fragment.app.viewModels
+import com.example.authorization.R
+import com.example.authorization.common.ext.setErrorFieldByRes
+import com.example.authorization.common.ext.toast
 import com.example.authorization.databinding.FragmentLoginBinding
 import com.example.authorization.presentation.base.BaseFragment
+import com.google.android.material.textfield.TextInputLayout
+import dagger.hilt.android.AndroidEntryPoint
 
-class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate)
+@AndroidEntryPoint
+class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
+
+    private val viewModel: LoginViewModel by viewModels()
+
+    private val requiredFields: List<TextInputLayout> by lazy {
+        with(binding) {
+            listOf(etLogin, etPassword)
+        }
+    }
+
+    override fun initListeners() = with(binding) {
+        super.initListeners()
+
+        imgBack.setOnClickListener { goBack() }
+
+        requiredFields
+            .forEach { layout ->
+                layout.editText?.doAfterTextChanged {
+                    if (layout.isErrorEnabled) {
+                        layout.setErrorFieldByRes(requireContext(), null)
+                    }
+                }
+            }
+
+        btnRegister.setOnClickListener {
+
+            requiredFields
+                .filter { it.editText?.text.isNullOrEmpty() && it.editText?.text.isNullOrBlank() }
+                .takeIf { it.isNotEmpty() }
+                ?.forEach { layout ->
+                    layout.setErrorFieldByRes(requireContext(), R.string.required)
+                } ?: run {
+                viewModel.login(
+                    etLogin.editText?.text.toString(),
+                    etPassword.editText?.text.toString()
+                )
+                toast("Login")
+            }
+
+        }
+    }
+
+
+}
